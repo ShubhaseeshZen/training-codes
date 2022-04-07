97edaeb2688a224996f31855872b8fe2c5480404
Shubhaseesh Kumar <s.kumar4@zensar.com>
2022-04-07 13:54:28 +0530

    added training projects as Praveen told

---
+++ day12AsyncAwait/Apis/worker.js
@@ -0,0 +1,8 @@
+let count1 = 0;
+
+function timecount () {
+    count1++;
+    
+postMessage(count1);
+setTimeout("timecount()", 2000);
+}
---
+++ day8/car.js
@@ -0,0 +1,6 @@
+
+let car1 = "Mahindra";
+let car2 = "Tata";
+
+//export {car1, car2};
+export {car1, car2};
